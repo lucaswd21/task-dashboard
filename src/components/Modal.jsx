8d42a2922234f454180b62541ad62cdc754f63df
Lucas Dias <lucas.wd.wd@gmail.com>
2024-08-24 01:18:10 -0300

    fixed responsibility

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,64 +1,65 @@
-import { useState } from "react";
 import { PlusIcon } from '@heroicons/react/24/outline'
 
-export default function Modal ({ modalDetails, task, setTask, taskHandler, isOpen, confirmCreate, closeModal }) {
+export default function Modal({ modalDetails, task, setTask, taskHandler, isOpen, confirmCreate, closeModal }) {
 
-    return <div>
-        <div className="flex justify-center items-center mt-2">
-            <button
-                onClick={confirmCreate}
-                className="flex justify-center items-center px-4 py-2 my-4 text-white bg-blue-500 rounded hover:bg-blue-700"
-            >
-                <PlusIcon className="w-5 h-5 mr-2 text-white" />
-                Nova tarefa
-            </button>
-        </div>
+    return (
+        <div>
+            <div className="flex justify-center items-center mt-2">
+                <button
+                    onClick={confirmCreate}
+                    className="flex justify-center items-center px-4 py-2 my-4 text-white bg-blue-500 rounded hover:bg-blue-700"
+                >
+                    <PlusIcon className="w-5 h-5 mr-2 text-white" />
+                    Nova tarefa
+                </button>
+            </div>
 
-        {
-            isOpen && (
-                <div className="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50">
-                    <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-lg shadow-lg">
-                        <div className="flex items-center justify-between pb-3">
-                            <h3 className="text-lg font-medium">{ modalDetails.title }</h3>
-                            <button
-                            onClick={closeModal}
-                            className="text-gray-400 hover:text-gray-600"
-                            >
-                                X
-                            </button>
-                        </div>
-                        {
-                            modalDetails.operation !== 'delete' && (
-                                <div className="p-2">
-                                    <input
-                                        className="w-80 p-2 m-4 text-gray-700 bg-white border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                        placeholder="Digite o nome da tarefa..."
-                                        type="text"
-                                        onChange={(e) => setTask(e.target.value)}
-                                        value={task}
-                                    />
-                                </div>
+            {
+                isOpen && (
+                    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-auto bg-black bg-opacity-50">
+                        <div className="relative w-full max-w-lg p-4 mx-4 sm:mx-auto bg-white rounded-lg shadow-lg">
+                            <div className="flex items-center justify-between pb-3">
+                                <h3 className="text-lg font-medium">{modalDetails.title}</h3>
+                                <button
+                                    onClick={closeModal}
+                                    className="text-gray-400 hover:text-gray-600"
+                                >
+                                    X
+                                </button>
+                            </div>
+                            {
+                                modalDetails.operation !== 'delete' && (
+                                    <div className="p-2">
+                                        <input
+                                            className="w-80 p-2 m-4 text-gray-700 bg-white border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                            placeholder="Digite o nome da tarefa..."
+                                            type="text"
+                                            onChange={(e) => setTask(e.target.value)}
+                                            value={task}
+                                        />
+                                    </div>
 
-                            )
-                        }
+                                )
+                            }
 
-                        <div className="flex justify-end pt-2">
-                            <button
-                                onClick={closeModal}
-                                className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-700"
-                            >
-                                Cancelar
-                            </button>
-                            <button
-                                onClick={() => taskHandler(modalDetails.operation)}
-                                className="px-4 py-2 ml-2 text-white bg-blue-500 rounded hover:bg-blue-700"
-                            >
-                                { modalDetails.buttonLabel }
-                            </button>
+                            <div className="flex justify-end pt-2">
+                                <button
+                                    onClick={closeModal}
+                                    className="px-4 py-2 text-white bg-red-500 rounded hover:bg-red-700"
+                                >
+                                    Cancelar
+                                </button>
+                                <button
+                                    onClick={() => taskHandler(modalDetails.operation)}
+                                    className="px-4 py-2 ml-2 text-white bg-blue-500 rounded hover:bg-blue-700"
+                                >
+                                    {modalDetails.buttonLabel}
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )
-        }
-    </div>
+                )
+            }
+        </div>
+    );
 }
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -2,23 +2,25 @@ import { MinusCircleIcon, CheckCircleIcon, PencilSquareIcon, TrashIcon } from '@
 
 export default function TaskList({ task, concludeTask, confirmEdit, confirmDelete }) {
   return (
-    <div className="flex items-center justify-between p-2 mx-4 my-2 bg-white rounded shadow-md">
-      <button
-        className="flex-shrink-0 p-2 text-white bg-gray-300 rounded hover:bg-blue-700"
-        onClick={() => concludeTask(task.id)}
-      >
-        {task.done ? (
-          <CheckCircleIcon className="w-6 h-6 text-green-500" />
-        ) : (
-          <MinusCircleIcon className="w-6 h-6 text-gray-700" />
-        )}
-      </button>
+    <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between p-4 mx-4 my-2 bg-white rounded shadow-md">
+      <div className="flex items-center w-full sm:w-auto">
+        <button
+          className="flex-shrink-0 p-2 text-white bg-gray-300 rounded hover:bg-blue-700"
+          onClick={() => concludeTask(task.id)}
+        >
+          {task.done ? (
+            <CheckCircleIcon className="w-6 h-6 text-green-500" />
+          ) : (
+            <MinusCircleIcon className="w-6 h-6 text-gray-700" />
+          )}
+        </button>
+        
+        <h1 className={`flex-grow text-lg ml-4 ${task.done ? 'text-green-500' : 'text-gray-700'}`}>
+          {task.name}
+        </h1>
+      </div>
       
-      <h1 className={`flex-grow text-center text-lg ${task.done ? 'text-green-500' : 'text-gray-700'}`}>
-        {task.name}
-      </h1>
-      
-      <div className="flex-shrink-0 flex space-x-2">
+      <div className="flex justify-end mt-2 sm:mt-0 w-full sm:w-auto space-x-2">
         <button
           className="p-2 text-white bg-gray-300 rounded hover:bg-blue-700"
           onClick={() => confirmEdit(task.id)}
